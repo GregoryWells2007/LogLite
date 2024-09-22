d5f3bd09d8a8ea65569582da55abf945c0ebcec2
Gregory Wells <gregowells07@gmail.com>
2024-09-21 20:24:32 -0400

    Add logging to files

--- src/log.go
+++ src/log.go
@@ -1,7 +1,9 @@
 package log
-import(
+
+import (
 	"fmt"
 	"time"
+	"os"
 )
 
 const (
@@ -11,17 +13,26 @@ const (
 )
 
 const (
-	List = 0
-	Console = 1
+	List    = 0
+	File    = 1
+	Console = 2
 )
 
-var output_lists []*[]string;
-var output_console bool = false;
+var output_lists []*[]string
+var output_files []*os.File;
+var output_console bool = false
+
 func AddOutput(output_type int, a ...interface{}) {
-	if (output_type == List) {
-		output_lists = append(output_lists, a[0].(*[]string));
-	} else if (output_type == Console) {
-		output_console = true;
+	if output_type == List {
+		output_lists = append(output_lists, a[0].(*[]string))
+	} else if output_type == File {
+		file, err := os.Create(a[0].(string))
+		if err != nil {
+			panic(err);
+		}
+		output_files = append(output_files, file)
+	} else if output_type == Console {
+		output_console = true
 	}
 }
 
@@ -30,39 +41,47 @@ func AddOutput(output_type int, a ...interface{}) {
 %l = level
 %m = message
 */
-var output_format string = "[%t] %l: %m";
+var output_format string = "[%t] %l: %m"
 
 func SetFormat(format string) {
-	output_format = format;
+	output_format = format
 }
 
 func Write(level string, message string) {
-	var output string;
+	var output string
 
 	for i := 0; i < len(output_format); i++ {
-		if (string(output_format[i]) == "%") {
-			i++;
-			format := output_format[i];
-			if (format == 't') {
-				output += time.Now().Format("15:04:05");
-			} else if (format == 'l') {
-				output += level;
-			} else if (format == 'm') {
-				output += message;
+		if string(output_format[i]) == "%" {
+			i++
+			format := output_format[i]
+			if format == 't' {
+				output += time.Now().Format("15:04:05")
+			} else if format == 'l' {
+				output += level
+			} else if format == 'm' {
+				output += message
 			} else {
 				// Write(Error, "invalid log code\n");
-				return;
+				return
 			}
 		} else {
-			output += string(output_format[i]);
+			output += string(output_format[i])
 		}
-	}	
-	
-	for i := 0; i < len(output_lists); i++ {
-		*output_lists[i] = append(*output_lists[i], output); 
 	}
-	
-	if (output_console) {
-		fmt.Printf("%s\n", output);
+	output += "\n";
+
+	for i := 0; i < len(output_lists); i++ {
+		*output_lists[i] = append(*output_lists[i], output)
+	}
+	for i := 0; i < len(output_files); i++ {
+        file := output_files[i];
+
+		_, err := file.WriteString(output);
+		if err != nil {
+			panic(err);
+		}
+	}
+	if output_console {
+		fmt.Printf("%s", output)
 	}
 }
