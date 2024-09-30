27223ef85ee553520197798e2288f63cdfad1362
Gregory Wells <gregowells07@gmail.com>
2024-09-30 08:25:27 -0400

    trace spelled wrong

--- src/log.go
+++ src/log.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	Log      = "Log"
-	Trage    = "Trace"
+	Trace    = "Trace"
 	Info     = "Info"
 	Message  = "Message"
 	Warning  = "Warning"
@@ -15,54 +15,55 @@ const (
 	Critical = "Critical"
 )
 
-var contains_console_out bool = false;
-var console_index int = 0;
+var contains_console_out bool = false
+var console_index int = 0
+
 func AddOutputTarget(output_type int, a ...interface{}) *OutputTarget {
-	if (contains_console_out && output_type == Console) {
-		Write(Warning, "Attempting to add multiple console outputs [LogLite]");
-		return &OutputTargets[console_index];
+	if contains_console_out && output_type == Console {
+		Write(Warning, "Attempting to add multiple console outputs [LogLite]")
+		return &OutputTargets[console_index]
 	}
 
-	var new_target OutputTarget = NewOutputTarget();
+	var new_target OutputTarget = NewOutputTarget()
 
 	if output_type == List {
-		listOutput := &ListOutput{};
-		new_target.OutputStream = listOutput;
+		listOutput := &ListOutput{}
+		new_target.OutputStream = listOutput
 	} else if output_type == File {
 		for i := 0; i < len(OutputTargets); i++ {
-			if (OutputTargets[i].OutputStream.GetOutputType() == File) {
-				if (OutputTargets[i].OutputStream.(*FileOutput).OutputFileName == a[0].(string)) {
-					Write(Warning, "Attempting to add a file as an output target again, returning old target [LogLite]");
-					return &OutputTargets[i];
+			if OutputTargets[i].OutputStream.GetOutputType() == File {
+				if OutputTargets[i].OutputStream.(*FileOutput).OutputFileName == a[0].(string) {
+					Write(Warning, "Attempting to add a file as an output target again, returning old target [LogLite]")
+					return &OutputTargets[i]
 				}
 			}
 		}
 
-		fileOutput := &FileOutput{};
-		new_target.OutputStream = fileOutput;
+		fileOutput := &FileOutput{}
+		new_target.OutputStream = fileOutput
 	} else if output_type == Console {
-		consoleOutput := &ConsoleOutput{};
-		new_target.OutputStream = consoleOutput;
-		contains_console_out = true;
-		console_index = len(OutputTargets);
+		consoleOutput := &ConsoleOutput{}
+		new_target.OutputStream = consoleOutput
+		contains_console_out = true
+		console_index = len(OutputTargets)
 	} else {
-		Write(Error, "Unknown Output target [LogLite]");
-		return nil;
+		Write(Error, "Unknown Output target [LogLite]")
+		return nil
 	}
 
-	OutputTargets = append(OutputTargets, new_target);
+	OutputTargets = append(OutputTargets, new_target)
 
-	OutputTargets[len(OutputTargets) - 1].OutputStream.Init(a...);
-	return &OutputTargets[len(OutputTargets) - 1];
+	OutputTargets[len(OutputTargets)-1].OutputStream.Init(a...)
+	return &OutputTargets[len(OutputTargets)-1]
 }
 func RemoveOutputTarget(target *OutputTarget) {
 	for i := 0; i < len(OutputTargets); i++ {
-		if (OutputTargets[i] == *target) {
+		if OutputTargets[i] == *target {
 			OutputTargets = append(OutputTargets[:i], OutputTargets[i+1:]...)
-			return;
+			return
 		}
 	}
-	WriteFormatted(Warning, "cannot removed output target as it is not a current output target [LogLite]");
+	WriteFormatted(Warning, "cannot removed output target as it is not a current output target [LogLite]")
 }
 
 /*
@@ -73,19 +74,19 @@ func RemoveOutputTarget(target *OutputTarget) {
 */
 
 func Write(level string, message string) {
-	if (len(OutputTargets) == 0) {
-		AddOutputTarget(Console);
-		Write(Warning, "No output target specified adding console [LogLite]");
+	if len(OutputTargets) == 0 {
+		AddOutputTarget(Console)
+		Write(Warning, "No output target specified adding console [LogLite]")
 	}
 
 	for i := 0; i < len(OutputTargets); i++ {
-		var output string = FormatMessage(OutputTargets[i].OuputPattern, []Insert{ Insert{'d', GetDate()}, Insert{'t', time.Now().Format("15:04:05")}, Insert{'l', level}, Insert{'m', message}  });
-		output += "\n";
-		OutputTargets[i].OutputStream.Output(output);
+		var output string = FormatMessage(OutputTargets[i].OuputPattern, []Insert{Insert{'d', GetDate()}, Insert{'t', time.Now().Format("15:04:05")}, Insert{'l', level}, Insert{'m', message}})
+		output += "\n"
+		OutputTargets[i].OutputStream.Output(output)
 	}
 }
 
 func WriteFormatted(level string, format string, a ...interface{}) {
-	var formatted_string string = fmt.Sprintf(format, a...);
-	Write(level, formatted_string);	
+	var formatted_string string = fmt.Sprintf(format, a...)
+	Write(level, formatted_string)
 }
